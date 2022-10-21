5ab6b4c6706e17bc7f2a4478453f0ae094f7541d
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2022-10-20 20:37:30 -0400

    Time: 23 ms (60.68%), Space: 8.9 MB (40.27%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -5,26 +5,20 @@ bool canConstruct(std::string ransomNote, std::string magazine) {
     std::map<char, int> letters;
     std::map<char,int>::iterator it;
 
-
-    for (auto& letter: ransomNote) {
-        it = letters.find(letter);
-        if(it->first == letter){
-            it->second--;
-        } else{
-            letters[letter] = -1;
-
-        }
-    }
-
     for (auto& letter: magazine) {
         it = letters.find(letter);
         if(it->first == letter){
             it->second++;
+        } else{
+            letters[letter] = 1;
         }
     }
 
-    for (auto& letterFr: letters) {
-        if(letterFr.second < 0){
+    for (auto& letter: ransomNote) {
+        it = letters.find(letter);
+        if(it->first == letter && it->second != 0){
+            it->second--;
+        } else{
             return false;
         }
     }
