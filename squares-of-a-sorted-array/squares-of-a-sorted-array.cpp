8865d5a89a4c287f9fabffc2654f5d3df6d65460
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2022-10-24 21:03:23 -0400

    Time: 91 ms, Memory: 25.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        
-        for(auto &n: nums){
-            n = n*n;
-        }
-        
-        sort(nums.begin(), nums.end());
-        
-        return nums;
+vector<int> sortedSquares(vector<int>& nums) {
+
+    vector<int> numsList(nums.size());
+    int low = 0;
+    int high = nums.size()-1;
+    int i = high;
+
+    while(high >= low){
+       if(abs(nums[high]) >= abs(nums[low])){
+           numsList[i] = nums[high] * nums[high];
+           high--;
+       } else{
+           numsList[i] = nums[low] * nums[low];
+           low++;
+       }
+       i--;
     }
+    return numsList;
+}
 };
