e629bc664fd297808d7c14a53c28469f24b6efd3
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2022-11-03 17:42:02 -0400

    Time: 23 ms, Memory: 10.1 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -3,11 +3,9 @@ public:
 bool checkIfExist(vector<int>& arr) {
 
     int cur {};
-    int j {};
-    int size = arr.size();
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < arr.size(); i++){
         cur = arr[i] * 2;
-        for (j = 0; j < size; j++){
+        for (int j = 0; j < arr.size(); j++){
             if (arr[j] == cur && j != i){
                 return true;
             }
