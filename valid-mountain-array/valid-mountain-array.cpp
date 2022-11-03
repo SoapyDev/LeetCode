d57c35631725c21f5ba785b4ca066c07661ab5db
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2022-11-03 19:46:56 -0400

    Time: 27 ms, Memory: 22.4 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.cpp
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -5,15 +5,20 @@ bool validMountainArray(vector<int>& arr) {
 
     int n = arr.size();
 
-    if (n < 3 || arr[0] >= arr[1] || arr[n-1] >= arr[n-2]){
+    if (n < 3){
+        return false;
+    }else if (arr[0] >= arr[1]){
+        return false;
+    } else if (arr[n-1] >= arr[n-2]){
         return false;
     }
 
     int i {1};
+
     while (arr[i] < arr[i+1] && i < n){
         i++;
     }
-
+    
     for (i++; i < n ; ++i) {
         if (arr[i] >= arr[i-1]){
             return false;
