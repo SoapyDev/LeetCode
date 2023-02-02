990e69719cd27b06e4a4ad6f1c75307dc9e870cf
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2023-02-02 18:26:08 -0500

    Time: 21 ms, Memory: 3.1 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut nums = nums;
+        nums.sort();
+
+        for i in 1..nums.len() {
+            if nums[i] == nums[i-1]  {
+                return true;
+            }
+        }
+        false
+    }
+}
