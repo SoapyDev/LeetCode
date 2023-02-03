f22505c5c20889c141076a8f2602aa9b53266ffa
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2023-02-02 20:57:13 -0500

    Time: 1 ms, Memory: 2.1 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.rs
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.rs
@@ -3,18 +3,18 @@ use std::collections::HashMap;
 impl Solution {
     pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
         
-        let capacity = if nums1.len() >= nums2.len()  { nums1.len() }else { nums2.len() };
-        let mut map = HashMap::with_capacity(capacity);
-
-        for &num in &nums1{
-            *map.entry(num).or_insert(0) += 1;
-        }
-
+        //create a hashmap with a capacity equal to nums1
+        let mut map = HashMap::with_capacity(nums1.len());
         let mut result:Vec<i32> = Vec::new();
 
+        //fill the hashmap and add 1 for every duplicate
+        nums1.into_iter().for_each(|val| *map.entry(val).or_insert(0) +=1);
+
+        // go through nums2 and if num is in map and the value is greater than 0, push to result and
+        // decrement value by 1;
         for num in nums2 {
             if map.get(&num) > Some(&0) {
-                map.entry(num).and_modify(|e| { *e -= 1 });
+                map.entry(num).and_modify(|v| { *v -= 1 });
                 result.push(num);
             }
         }
