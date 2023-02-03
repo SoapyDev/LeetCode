7d2040e7790a19cd98fae2db602876222b4587bd
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2023-02-02 19:18:53 -0500

    Time: 3 ms, Memory: 2.2 MB - LeetHub

--- single-number/single-number.rs
+++ single-number/single-number.rs
@@ -2,21 +2,7 @@ use std::collections::{HashMap};
 impl Solution {
     
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut map:HashMap<i32, i32> = HashMap::with_capacity(nums.len() / 2 + 1);
-
-        for num in nums {
-            if let std::collections::hash_map::Entry::Vacant(e) = map.entry(num) {
-                e.insert(1);
-            } else {
-                map.remove(&num);
-            }
-        }
-
-        for (key, value) in map.iter(){
-            if *value == 1{
-                return *key;
-            }
-        }
-        0
+        //use the fold operator to make a bitwise xor operation on the accumulator (acc)
+        nums.iter().fold(0,|acc, x| acc^x)
     }
 }
