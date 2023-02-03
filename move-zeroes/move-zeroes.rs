5bc07cb534d0c3ff9b96d4aa56607ade47798406
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2023-02-03 13:47:29 -0500

    Time: 6 ms, Memory: 2.5 MB - LeetHub

--- move-zeroes/move-zeroes.rs
+++ move-zeroes/move-zeroes.rs
@@ -3,25 +3,18 @@ impl Solution {
 
     pub fn move_zeroes(mut nums: &mut Vec<i32>) {
 
-        let mut end = if nums.len() > 1 { 1 } else { return; };
-        let mut start = 0;
-
-     while end < nums.len(){
-            while  end < nums.len() && nums[end] == 0 {
-                end += 1;
-            }
-            while start < nums.len() &&  nums[start] != 0 {
-                start += 1;
-                if start == end {
-                    end += 1;
+         let mut pointer = 0;
+            let mut temp;
+            for index in 0..nums.len() {
+                if nums[pointer] == 0 {
+                    temp = nums[index];
+                    nums[index] = nums[pointer];
+                    nums[pointer] = temp;
+                }
+                if nums[pointer] != 0 {
+                    pointer += 1;
                 }
             }
-
-            if end < nums.len() && nums[end] != 0 && nums[start] == 0 {
-                nums[start] = nums[end];
-                nums[end] = 0;
-            }
-        }
 
     }
 }
