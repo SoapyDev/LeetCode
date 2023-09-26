8d59c3c5bb8bbe229e2d9e0854c31448cfcb3616
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2023-09-26 15:39:27 -0400

    Time: 424 ms, Memory: 4 MB - LeetHub

--- 000k-radius-subarray-averages/000k-radius-subarray-averages.rs
+++ 000k-radius-subarray-averages/000k-radius-subarray-averages.rs
@@ -1,18 +1,19 @@
 impl Solution {
     pub fn get_averages(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let k = k as usize;
-    let mut averages = Vec::with_capacity(nums.len());
+    let window = k * 2 + 1;
+    let len = nums.len();
+    let mut averages = vec![-1; len];
+    let temp_nums = nums.iter().map(|&x| x as usize).collect::<Vec<usize>>();
 
-    for i in 0..nums.len(){
-        if i < k || i >= nums.len() - k {
-            averages.push(-1);
+    for i in 0..len {
+        if i < k || i >= len - k {
             continue;
         }
-        let sum: usize = nums[i - k..=i + k].iter().map(|&x| x as usize).sum();
-        averages.push((sum / (2 * k + 1)) as i32);
+        let sum: usize = temp_nums[i - k..=i + k].iter().sum();
+        averages[i] = (sum / window) as i32;
     }
 
     averages
-
     }
 }
