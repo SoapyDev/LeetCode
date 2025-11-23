dd3a3b74fa827094d5605c38ee80065f24a4f14f
SoapyDev <89493790+SoapyDev@users.noreply.github.com>
2025-11-23 13:13:10 -0500

    Time: 0 ms (100.00%) | Memory: 2.3 MB (79.77%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.rs
@@ -0,0 +1,58 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Direction {
+    Ascending,
+    Descending,
+}
+
+
+impl Direction {
+    pub fn flip(&mut self) {
+        *self = match self {
+            Direction::Ascending => Direction::Descending,
+            Direction::Descending => Direction::Ascending,
+        };
+    }
+
+    pub fn is_descending(&self) -> bool {
+        *self == Direction::Descending
+    }
+
+    pub fn is_ascending(&self) -> bool {
+        *self == Direction::Ascending
+    }
+
+}
+
+
+impl Solution {
+
+
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let num_rows = num_rows as usize;
+
+        if num_rows == 1 {
+            return s;
+        }
+
+        let mut words: Vec<String> = vec![String::new(); num_rows];
+        let mut row = 0;
+        let mut direction = Direction::Descending;
+
+        for c in s.chars() {
+            words.get_mut(row).map(|word| word.push(c));
+
+            if row == num_rows - 1 {
+                direction = Direction::Ascending;
+            } else if row == 0 {
+                direction = Direction::Descending;
+            }
+
+            match direction {
+                Direction::Ascending => row -= 1,
+                Direction::Descending => row += 1,
+            }
+        }
+        words.join("")
+    }
+
+}
